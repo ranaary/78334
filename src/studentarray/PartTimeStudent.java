bf7bffa1a83eff9d2f176be6a0ddfbf134426412
megha <megha.patel2@sheridancollege.ca>
2022-01-31 11:20:30 -0500

    parttimestudent.java added

---
+++ src/studentarray/PartTimeStudent.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package studentarray;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class PartTimeStudent {
+
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    private int numOfCourses;
+}
